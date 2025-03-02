69eb61195e4674c67ac1a5e27178de42f5d34575
Dipesh Adhikari <dipeshadh24@gmail.com>
2025-03-02 19:48:45 +0545

    sign in page modified

--- src/pages/Signin.jsx
+++ src/pages/Signin.jsx
@@ -1,49 +1,37 @@
 import useAuthForm from "../hooks/AuthForm";
 import { FormInput } from "../components/FormInput";
 import { Link } from "react-router-dom";
-import { supabase } from "../lib/supabaseClient.js";
-import { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useState, useContext } from "react";
+import { AuthContext } from "../context/AuthContext";
 
 const Signin = () => {
-  const navigate = useNavigate()
   const { register, handleSubmit, errors } = useAuthForm();
-  const [loading, setLoading] = useState(false);
   const [errorMessage, setErrorMessage] = useState("");
-  const [successMessage, setSuccessMessage] = useState("");
+  const {signin, signInWithGithub, loading} = useContext(AuthContext)
 
   const onSubmit = async (formData) => {
-    setLoading(true);
-    setErrorMessage("");
-    setSuccessMessage("");
-
-    const response = await supabase.auth.signInWithPassword({
-      email: formData.email,
-      password: formData.password,
-    });
-    console.log("Response: ", response)
-
-    const {data, error} = response
-
-    if (error) {
-      setErrorMessage(error.message);
-    } else {
-      console.log(data)
-      setSuccessMessage("You have successfully signed in");
-      setTimeout(()=>{
-        navigate("/dashboard")
-      },2000)
-
+    try {
+      setErrorMessage("")
+      await signin(formData.email, formData.password)
+    } catch (error) {
+      setErrorMessage(error.message)
     }
-    setLoading(false);
+
   };
+
+  const  githubSignIn = async () => {
+    try {
+      signInWithGithub()
+    } catch (error) {
+      setErrorMessage(error.message)
+    }
+  }
   return (
     <div className="h-auto   w-5xl mx-auto mt-24 border p-4 shadow-2xl">
       <div className="flex flex-col items-center justify-center">
         <h1 className="text-3xl font-bold mb-8">Sing In</h1>
         <div>
           {errorMessage && <p className="text-red-600">{errorMessage}</p>}
-          {successMessage && <p className="text-green-500">{successMessage}</p>}
           <form onSubmit={handleSubmit(onSubmit)}>
             <FormInput
               label="Email"
@@ -70,6 +58,17 @@ const Signin = () => {
               {loading ? "Signing in...." : "Sign In"}
             </button>
           </form>
+          <div className="mt-4">
+            <p className="text-md">Or sign up with</p>
+
+            <button
+              className="border shadow-2xl p-2 hover:cursor-pointer w-80 "
+              type="submit"
+              onClick={githubSignIn}
+            >
+              {loading ? "Signing up...." : "Github"}
+            </button>
+          </div>
           <p className="text-md my-4 ">
             Don`&apos;`t have an account?{" "}
             <Link className="text-blue-700" to="/signup">
